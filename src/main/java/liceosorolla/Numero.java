a20a86dd3758ea7e514ff76583ae95e59eed6075
DAM_1 <DAM_1@PB09>
2020-01-24 21:10:58 +0100

    factorial mejora-5

--- src/main/java/liceosorolla/Numero.java
+++ src/main/java/liceosorolla/Numero.java
@@ -4,6 +4,18 @@ public class Numero {
 
 	public static void main(String[] args) {
 		int numero=9;
+		int factorial=factorial(numero);
+		System.out.println("El factorial de "+9+" es "+factorial);
+	}
+	public static int factorial(int numero) {
+		int contador=1;
+		int factorial=1;
+		while(contador<=numero){
+			factorial = factorial*contador;
+			
+			contador++;
+		}
+		return factorial;
 	}
 
 }
