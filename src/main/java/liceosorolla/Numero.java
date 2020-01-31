2a8502486055e0a3c3acdd80c5f0bc079fcc004b
DAM_1 <DAM_1@PB09>
2020-01-31 19:35:05 +0100

    Mejora-4

--- src/main/java/liceosorolla/Numero.java
+++ src/main/java/liceosorolla/Numero.java
@@ -6,16 +6,30 @@ public class Numero {
 		int numero=9;
 		int factorial=factorial(numero);
 		System.out.println("El factorial de "+9+" es "+factorial);
+		System.out.println("¿El numero "+numero+" es primo? "+numeroPrimo(numero));
 	}
 	public static int factorial(int numero) {
 		int contador=1;
 		int factorial=1;
 		while(contador<=numero){
 			factorial = factorial*contador;
-			
 			contador++;
 		}
 		return factorial;
 	}
-
+	public static boolean numeroPrimo(int numero) {
+		int primo=0;
+		boolean bool=false;
+		for(int i=1;i<numero;i++) {
+			if(numero%i==0) {
+				primo++;
+			}
+		}
+		if(primo>1) {
+			bool=false;
+		}else {
+			bool=true;
+		}
+		return bool;
+	}
 }
