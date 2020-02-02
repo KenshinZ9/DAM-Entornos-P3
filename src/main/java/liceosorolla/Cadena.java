213653ee650f05b900f83164e314be8246d2ed99
Familia Ojeda <Familia Ojeda@DESKTOP-5UIP68O>
2020-02-02 23:33:29 +0100

    Mejora_7

--- src/main/java/liceosorolla/Cadena.java
+++ src/main/java/liceosorolla/Cadena.java
@@ -4,6 +4,10 @@ public class Cadena {
 
 	public static void main(java.lang.String[] args) {
 		String cadena="Hola soy perico ";
+		mayusminus(cadena);
 	}
-
+public static void mayusminus(String cadena) {
+	System.out.println("La frase en minusculas será: "+cadena.toLowerCase()); 
+    System.out.println("La frase en mayusculas será: "+cadena.toUpperCase()); 
+}
 }
